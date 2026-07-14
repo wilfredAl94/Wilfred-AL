2e5d2ec3eda9c0ce7827bb5f93521e798a853040
Wilfred _ AL <wilfredojo002@gmail.com>
2026-07-14 18:39:32 +0100

    Change API endpoint for model inference

--- api/generate.js
+++ api/generate.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
     const { prompt } = req.body;
 
     const response = await fetch(
-      "https://api-inference.huggingface.co/models/black-forest-labs/FLUX.1-dev",
+      "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-2-1",
       {
         method: "POST",
         headers: {
